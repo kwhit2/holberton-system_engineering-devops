e842e947c00d5058db0529aa11d98200b7114388
kwhit2 <kfw2@outlook.com>
2021-04-05 00:51:12 +0000

    Maybe

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Using Puppet, configures SSH config file so that you can connect to a...
+# ...server without typing a password.
+# SSH client configuration configured to use the private key ~/.ssh/holberton
+# SSH client configuration configured to refuse to authenticate using a passwd
+
+include stdlib
+file_line { 'holbie private key':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'remove pwd authentication':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+}
