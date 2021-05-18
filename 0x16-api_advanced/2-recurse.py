3e8d728799a29ec3e5ca02fc3da4e3a8bf461b89
kwhit2 <kfw2@outlook.com>
2021-05-18 18:27:31 +0000

    Take set-up: no code

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit. If no results are
+    found for the given subreddit, the function should return None """
+import requests
+from sys import argv
+
+
+def recurse(subreddit, hot_list=[]):
+    """ recurse method """
+    
