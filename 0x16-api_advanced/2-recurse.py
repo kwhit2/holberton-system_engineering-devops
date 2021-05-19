be5ab657810778dadf8b06fd51a84fbe06948fbc
kwhit2 <kfw2@outlook.com>
2021-05-19 14:58:17 +0000

    Import and pep8 Fix: No Code Added yet

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,10 +2,10 @@
 """recursive function that queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit. If no results are
     found for the given subreddit, the function should return None """
+import json
 import requests
 from sys import argv
 
 
 def recurse(subreddit, hot_list=[]):
     """ recurse method """
-    
